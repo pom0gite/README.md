7810232f46e46b2b80ec6901201a6c3addf1c811
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-05-02 23:16:58 +0300

    Create NFT.sol

---
+++ NFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Simple NFT contract for Base mini project
+
+contract NFT {
+    string public name = "BaseNFT";
+    string public symbol = "BNFT";
+
+    // TODO: implement mint NFT
+    // TODO: add token URI
+}
