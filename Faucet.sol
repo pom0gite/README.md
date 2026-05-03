a373b2cbdc08c1661361abd6ecbab5a47822540a
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-05-03 18:41:11 +0300

    Update Faucet.sol

--- Faucet.sol
+++ Faucet.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// Faucet contract for Base project
+
+contract Faucet {
+
+    // TODO: connect to Token contract
+    // TODO: implement claim function
+    // TODO: add cooldown between claims
+
+}
