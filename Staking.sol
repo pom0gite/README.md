b6274f16fdd96a05c8dc5ab8cc1d6594da8894ad
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-05-04 16:22:28 +0300

    Update Staking.sol

--- Staking.sol
+++ Staking.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// Staking contract for Base project
+
+contract Staking {
+
+    // TODO: stake tokens
+    // TODO: unstake tokens
+    // TODO: calculate rewards
+
+}
